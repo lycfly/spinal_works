917c4ceee242fb9011d09d0562a43f468673e9a6
lycfly <601753574@qq.com>
2022-01-27 18:02:13 +0800

    t

---
+++ cardiff-b0/src/main/scala/PiecewiseApproximator.scala
@@ -0,0 +1,45 @@
+
+import spinal.sim._
+import spinal.core._
+import spinal.core.sim._
+import spinal.lib._
+import spinal.lib.fsm._
+import scala.util.Random
+import scala.language.postfixOps
+import scala.math._
+
+class PiecewiseApproximator (SizeIn: Int, SizeOut: Int) extends Component {
+  val io = new Bundle {
+    val en = in Bool()  // dot finish
+    val rg_cordic_iternum = in UInt(3 bits)
+    val vld = in Bool()
+    val y = in SInt(SizeIn bits)
+    val x = in SInt(SizeIn bits)
+    val res = out SInt(SizeOut bits)
+    val res_vld = out Bool()
+
+  }
+  noIoPrefix()
+  val Stage = 7
+  val logStage = log2Up(Stage)
+  def op(a:Int,b:Int):Int={
+    a + b
+  }
+  val test = Runtime.getRuntime.
+
+
+
+
+}
+
+object PiecewiseApproximatorInst {
+  def main(args: Array[String]): Unit = {
+    SpinalConfig(
+      defaultConfigForClockDomains = ClockDomainConfig(resetKind = ASYNC,
+        clockEdge = RISING,
+        resetActiveLevel = LOW),
+      mode=Verilog)
+      .addStandardMemBlackboxing(blackboxAll)
+      .generate(new PiecewiseApproximator (SizeIn = 8, SizeOut = 8))
+  }.printPruned()
+}
